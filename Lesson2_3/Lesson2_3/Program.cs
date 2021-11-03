33c99767c348a5da8cf7d9c7ded31c20e82f6400
Arif Hasanov <arifhasanov@yahoo.com>
2021-11-03 13:41:58 +0100

    init

--- Lesson2_3/Lesson2_3/Program.cs
+++ Lesson2_3/Lesson2_3/Program.cs
@@ -6,7 +6,7 @@ namespace Lesson2_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
         }
     }
 }
